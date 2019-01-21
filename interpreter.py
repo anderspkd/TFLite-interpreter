b40989b3acf708c75e1a03d3b9e656bebc3e9e41
Anders Dalskov <anderspkd@fastmail.com>
2019-01-21 04:25:43 +0100

    Working on depthwise_conv2d. Not finished.

--- interpreter.py
+++ interpreter.py
@@ -1,5 +1,6 @@
 import numpy as np
 from model import TFLiteModel
+import quantization
 
 # This "interpreter" is purely for illustrative purposes. It goes through the
 # entire model, passing the input to the output at each layer. It serves to
@@ -41,6 +42,13 @@ def run_interactive_no_eval(model_path, data):
     print model.get_output().data
 
 
+def determine_padding_dims(padding, filter_shape, input_shape):
+    # compute height and width needed for padding according to `padding` (either
+    # SAME or VALID), where `input_shape` is the shape of the input and
+    # `filter_shape` is the shape of the kernel.
+    pass
+
+
 # this is sorta hacky. But I'm not sure how else to determine where a specific
 # tensor is used.
 def is_bias_tensor(tensor):
@@ -76,6 +84,10 @@ def split_conv2d_inputs(inputs):
     return weights, bias, data
 
 
+def offset(shape, i0, i1, i2, i3):
+    return ((i0 * shape[1] + i1) * shape[2] + i2) * shape[3] + i3
+
+
 float32 = np.float32
 int32 = np.int32
 uint8 = np.uint8
@@ -94,11 +106,11 @@ def conv2d(op, inputs, output):
 
     print 'Computing Conv2D'
 
-    batches, input_height, input_width, input_channels = data.shape
-    filter_height, filter_width, _, output_channels = weights.shape
-    _, output_height, output_width, _ = output.shape
-    stride_h, strid_w = op.stride
-    dilation_h, dilation_w = op.dilation_factor
+    # batches, input_height, input_width, input_channels = data.shape
+    # filter_height, filter_width, _, output_channels = weights.shape
+    # _, output_height, output_width, _ = output.shape
+    # stride_h, stride_w = op.stride
+    # dilation_h, dilation_w = op.dilation_factor
 
     # filter_offset is weight quantization
     # input_offset is likewise input quantization
@@ -114,20 +126,21 @@ def conv2d(op, inputs, output):
     # input. In particular that there's only ever going to be one.
 
     # lmao this is slow as fuck
-    for b in range(batches):
-        for out_y in range(output_height):
-            for out_x in range(output_width):
-                for out_c in range(output_channels):
-                    # TODO: figure out padding
-                    in_x_origin = (out_x * stride_w)
-                    in_y_origin = (out_y * stride_h)
-                    int32_acc = int32(0)
-                    for filter_y in range(filter_height):
-                        for filter_x in range(filter_width):
-                            for in_c in range(input_channels):
-                                in_y_origin + dilation_h * filter_y
-                                if (0 <= in_x < input_w) and (0 <= in_y < input_h):
-                                    pass
+    # for b in range(batches):
+    #     for out_y in range(output_height):
+    #         for out_x in range(output_width):
+    #             for out_c in range(output_channels):
+    #                 # TODO: figure out padding
+    #                 in_x_origin = (out_x * stride_w)
+    #                 in_y_origin = (out_y * stride_h)
+    #                 int32_acc = int32(0)
+    #                 for filter_y in range(filter_height):
+    #                     for filter_x in range(filter_width):
+    #                         for in_c in range(input_channels):
+    #                             in_x = in_x_origin + dilation_w * filter_x
+    #                             in_y = in_y_origin + dilation_h * filter_y
+    #                             if (0 <= in_x < input_w) and (0 <= in_y < input_h):
+    #                                 pass
                             # end in_c
                         # end filter_x
                     # end filter_Y
@@ -141,7 +154,74 @@ def conv2d(op, inputs, output):
 
 
 def depthwise_conv2d(op, inputs, output):
+
+    weights, bias, input_data = split_conv2d_inputs(inputs)
+
+    num_batches, input_h, input_w, input_c = input_data.shape
+    filter_h, filter_w, _, output_c = weights.shape
+    _, output_h, output_w, _ = output.shape
+    stride_h, stride_w = op.stride
+    dilation_h, dilation_w = op.dilation_factor
+    depth_multiplier = op.depth_multiplier
+
+    filter_s = weights.scale
+    filter_z = weights.zero_point
+    input_s = input_data.scale
+    input_z = input_data.zero_point
+    output_s = output.scale
+    output_z = output.zero_point
+
+    # padding is a function of the shape of the input and outptu
+    pad_h, pad_w = determine_padding_dims(
+        op.padding, weights.shape, input_data.shape)
+
+    # compute the shift and multiplier once and for all here.
+    n, qm = quantization.compute_multiplier(filter_s, input_s, output_s)
+
     print 'Computing Depthwise Conv2D'
+
+    for b in range(num_batches):
+        for out_y in range(output_h):
+            for out_x in range(output_w):
+                for ic in range(input_c):
+                    for m in range(depth_multiplier):
+                        oc = m + ic * depth_multiplier
+                        # pad width and height here
+                        in_x_origin = (out_x * stride_w) - 0
+                        in_y_origin = (out_y * stride_h) - 0
+                        acc = int32(0)
+                        for filter_y in range(filter_h):
+                            for filter_x in range(filter_w):
+                                in_x = in_x_origin + dilation_w * filter_x
+                                in_y = in_y_origin + dilation_h * filter_y
+
+                                if (0 <= in_x < input_w and 0 <= in_y < input_h):
+                                    iv = input_data[offset(
+                                        input_data.shape,
+                                        b, in_y, in_x, ic
+                                    )]
+                                    fv = weights[offset(
+                                        weights.shape,
+                                        0, filter_y, filter_x, oc
+                                    )]
+                                    acc += int32((fv + filter_s) * (iv + input_s))
+                            # end filter_x
+                        # end filter_y
+                        acc += int32(bias[oc])
+
+                        # multiply by M (which we have as the pair (n, qm)). We
+                        # want to get readings that are accurate, so we cannot
+                        # simply multiply the original M here. We have to go
+                        # through all the hoops that gemmlowp also does.
+                        acc = quantized_multiplier_mult(acc, qm, n)
+
+                    # end m
+                # end ic
+            # end out_x
+        # end out_y
+    # end b
+
+    #
 
 
 def add(op, inputs, output):
@@ -164,21 +244,17 @@ def run(model, input_data):
         op_inputs = [model.tensors[idx] for idx in op.inputs]
         op_outputs = [model.tensors[idx] for idx in op.outputs]
 
-        # assume all operations have one output only
-        assert len(op_outputs) == 1, 'Cannot handle more than 1 output'
-        output_tensor = op_outputs[0]
-
         opname = op.opname
         if 'CONV_2D' == opname:
-            output = conv2d(op, op_inputs, output_tensor)
+            conv2d(op, op_inputs, op_outputs)
         elif 'DEPTHWISE_CONV_2D' == opname:
-            output = depthwise_conv2d(op, op_inputs, output_tensor)
+            depthwise_conv2d(op, op_inputs, op_outputs)
         elif 'ADD' == opname:
-            output = add(op, op_inputs, output_tensor)
+            add(op, op_inputs, op_outputs)
         elif 'AVERAGE_POOL_2D' == opname:
-            output = avgpool2d(op, op_inputs, output_tensor)
+            avgpool2d(op, op_inputs, op_outputs)
         elif 'RESIZE_BILINEAR' == opname:
-            output = resize_bilinear(op, op_inputs, output_tensor)
+            resize_bilinear(op, op_inputs, op_outputs)
         else:
             print 'Unknown operator: %s' % (opname,)
             return
