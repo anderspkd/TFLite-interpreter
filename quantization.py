240bf77dfd7d9ba6b9c1aa7f7cc1556e1c15bd65
Anders Dalskov <anderspkd@fastmail.com>
2019-01-21 23:07:15 +0100

    Fixed quantization code + test.

--- quantization.py
+++ quantization.py
@@ -1,5 +1,5 @@
 import numpy as np
-# import Decimals
+import decimal
 
 int32 = np.int32
 int64 = np.int64
@@ -10,14 +10,9 @@ INT32_MAX = int32(INT32_MIN - 1)
 
 # handlers and helpers for quantization stuff
 def compute_multiplier(s1, s2, s3):
-    # given three FP32 values (corresponding to three quantization scale
-    # values), compute and return integers n and qm, so that
-    #
-    #   (s1*s2)/s3 = qm/2^n
-    #
-    # qm fits in a int32
+    # given rm = (s1*s2)/s3, compute qm, n, so that rm \approx qm >> n.
 
-    real_multiplier = (float(s1)*s2)/s3
+    real_multiplier = (s1*s2)/s3
 
     assert 0.0 < real_multiplier < 1.0, \
         'real_multiplier=%s (s1=%s, s2=%s, s3=%s)' % (
@@ -38,25 +33,27 @@ def compute_multiplier(s1, s2, s3):
     assert qm <= INT32_MAX
     assert n >= 0
 
-    return n, int32(qm)
+    return -n, int32(qm)
 
-# TODO: Figure out a way to test this guy
+
 def quantized_multiplier_mult(x, multiplier, shift):
     lshift, rshift = (shift, 0) if shift > 0 else (0, -shift)
-
     # 1. saturate mult between `x * (1 << lshift)` and `multiplier`
-    x *= 1 << lshift
-    overflow = (x == multiplier and x == INT32_MIN)
-    a = int64(x)
+    x_ = int32(x * (1 << lshift))
+    overflow = (x_ == multiplier and x == INT32_MIN)
+    a = int64(x_)
     a = a * int64(multiplier)
     nudge = (1 << 30) if a >= 0 else (1 - (1 << 30))
-    a = int32((a + nudge)/int64(1 << 31))
+    a = int32((a + nudge) / (1 << 31))
     a = INT32_MAX if overflow else a
 
     # 2. rounding to nearest
-    mask = (1 << shift) - 1
+    # exponent = rshift
+    mask = (1l << rshift) - 1
     remainder = a & mask
+    # MaskIfLessThan(a, 0) & 1
     bit1 = 1 if a < 0 else 0
     threshold = (mask >> 1) + bit1
+    # MaskIfGreaterThan(remainder, threshold) & 1
     bit2 = 1 if remainder > threshold else 0
-    return (a << shift) + bit2
+    return (a >> rshift) + bit2
--- test_quant_multiplier.py
+++ test_quant_multiplier.py
@@ -2,17 +2,22 @@
 
 import quantization
 import model
+import random
 import numpy as np
+from interpreter import is_bias_tensor, is_weights_tensor
 from sys import argv
 
-m = model.TFLiteModel(argv[1])
+m = model.TFLiteModel(argv[1], parse_data=False)
 
 for op in m:
     if op.opname not in ('CONV_2D', 'DEPTHWISE_CONV_2D'):
         continue
-    zps = [i.scale for i in m.get_inputs_for_op(op)]
+    ins = [m.tensors[idx] for idx in op.inputs if
+           is_bias_tensor(m.tensors[idx]) or is_weights_tensor(m.tensors[idx])]
+    outs = [i.scale for i in m.get_outputs_for_op(op)]
+    zps = [i.scale for i in ins] + outs
     n, qm = quantization.compute_multiplier(*zps)
-    print 'n=%s, qm=%s' % (n, qm)
-    print '%s ?= %s' % (
-        quantization.quantized_multiplier_mult(1, qm, n),
-        (zps[0]*zps[1])/zps[2])
+    large_num = pow(-1,random.randint(0,1)) * (1 << 25)
+    print '%s ?= %s (n=%s, qm=%s)' % (
+        quantization.quantized_multiplier_mult(large_num, qm, n),
+        large_num*((zps[0]*zps[1])/zps[2]), n, qm)
