a5b696b775d3f3e067bec186675b76f9302a4e01
Anders Dalskov <anderspkd@fastmail.com>
2019-01-29 23:48:10 +0100

    Non quantized version + a bunch of other stuff.

--- interpreter_v2.py
+++ interpreter_v2.py
@@ -5,11 +5,116 @@ import quantization
 import sys
 from PIL import Image
 
-if len(sys.argv) < 3:
-    print 'Usage: %s [tflite file] [image]' % (sys.argv[0],)
-    exit(0)
-
 mult_by_quant_mult = quantization.quantized_multiplier_mult
+
+int32 = np.int32
+int64 = np.int64
+
+def rounded_mult(v, multiplier):
+    return np.int64(round(multiplier * v))
+
+
+def dequantize_tensor(tensor):
+    S, Z = tensor.scale, tensor.zero_point
+    return (np.array(tensor.data, dtype='float64') - Z) * S
+
+
+def conv2d_dequant(options, inputs, weights, bias, output):
+    # assume input has already been dequantized.
+
+    weights = dequantize_tensor(weights)
+    bias = dequantize_tensor(bias)
+
+    # from here everything is essentially the same as before
+    n_channels_out, weights_h, weights_w, n_channels_in = weights.shape
+    _, inputs_h, inputs_w, n_channels_in_ = inputs.shape
+    _, output_h, output_w, n_channels_out_ = output.shape
+
+    assert n_channels_in_ == n_channels_in
+    assert n_channels_out_ == n_channels_out
+
+    stride_h, stride_w = options.stride
+    padding_h, padding_w = (weights_h // 2, weights_w // 2)
+
+    print 'input shape: ', inputs.shape
+    print 'output shape:', output.shape
+    print 'weights shape:', weights.shape
+
+    output.data = np.zeros(output.shape, dtype='float64')
+
+    # reference_ops.h version. Assume num_batch == 0
+    for out_y in range(output_h):
+        for out_x in range(output_w):
+            for out_c in range(n_channels_out):
+                in_x_origin = (out_x * stride_w) - padding_w
+                in_y_origin = (out_y * stride_h) - padding_h
+                acc = 0.0
+                for filter_y in range(weights_h):
+                    for filter_x in range(weights_w):
+                        for in_c in range(n_channels_in):
+                            in_x = in_x_origin + filter_x
+                            in_y = in_y_origin + filter_y
+                            if (0 <= in_x < inputs_w) and \
+                               (0 <= in_y < inputs_h):
+                                iv = inputs[0][in_y][in_x][in_c]
+                                wv = weights[out_c][filter_y][filter_x][in_c]
+                                acc += iv * wv
+                acc += bias[out_c]
+                # relu6
+                acc = 0.0 if acc < 0 else acc
+                acc = 6.0 if acc > 6 else acc
+                output.data[0][out_y][out_x][out_c] = acc
+
+    return output.data
+
+
+def dwconv2d_dequant(options, inputs, weights, bias, output):
+    # assume input has already been dequantized.
+
+    weights = dequantize_tensor(weights)
+    bias = dequantize_tensor(bias)
+
+    depth_multiplier = options.depth_multiplier
+
+    # from here everything is essentially the same as before
+    _, weights_h, weights_w, output_depth = weights.shape
+    _, inputs_h, inputs_w, n_channels_in = inputs.shape
+    _, output_h, output_w, output_depth_ = output.shape
+
+    assert output_depth_ == output_depth
+
+    stride_h, stride_w = options.stride
+    padding_h, padding_w = (weights_h // 2, weights_w // 2)
+
+    print 'input shape: ', inputs.shape
+    print 'output shape:', output.shape
+    print 'weights shape:', weights.shape
+
+    output.data = np.zeros(output.shape, dtype='float64')
+
+    for out_y in range(output_h):
+        for out_x in range(output_w):
+            for in_c in range(n_channels_in):
+                for m in range(depth_multiplier):
+                    oc = m + in_c * depth_multiplier
+                    in_x_origin = (out_x * stride_w) - padding_w
+                    in_y_origin = (out_y * stride_h) - padding_h
+                    acc = 0.0
+                    for filter_y in range(weights_h):
+                        for filter_x in range(weights_w):
+                            in_x = in_x_origin + filter_x
+                            in_y = in_y_origin + filter_y
+                            if (0 <= in_x < inputs_w) and \
+                               (0 <= in_y < inputs_h):
+                                iv = inputs[0][in_y][in_x][in_c]
+                                wv = weights[0][filter_y][filter_x][oc]
+                                acc += (iv * wv)
+                    acc += bias[oc]
+                    acc = 0.0 if acc < 0 else acc
+                    acc = 6.0 if acc > 6 else acc
+                    output[0][out_y][out_x][oc] = acc
+
+    return output.data
 
 
 def conv2d(options, inputs, weights, bias, output):
@@ -65,12 +170,12 @@ def conv2d(options, inputs, weights, bias, output):
                         xx = x + a - padding_h
                         yy = y + b - padding_w
                         if (0 <= xx < inputs_h) and (0 <= yy < inputs_w):
-                            z.append(inputs[0][xx][yy][c] - inputs_Z)
+                            z.append(int64(inputs[0][xx][yy][c] - inputs_Z))
                         else:
-                            z.append(0)
+                            z.append(int64(0))
                 Z.append(z)
         inputs_per_channel.append(Z)
-    inputs_per_channel = np.array(inputs_per_channel, dtype='int32')
+    inputs_per_channel = np.array(inputs_per_channel, dtype='int64')
 
     print 'done'
 
@@ -85,7 +190,7 @@ def conv2d(options, inputs, weights, bias, output):
             for a in range(weights_h):
                 for b in range(weights_w):
                     w.append(weights[c][a][b][c_] - weights_Z)
-            ww.append(np.array(w, dtype='int32'))
+            ww.append(np.array(w, dtype='int64'))
         flat_weights.append(ww)
 
     print 'done'
@@ -94,12 +199,21 @@ def conv2d(options, inputs, weights, bias, output):
     sys.stdout.flush()
 
     # This step is very cheap
+    max_print = 10
+    print_counter = 0
     output_list = list()
     for c in range(n_channels_out):
         output_for_c = np.zeros(output_shape[:-1])
         for c_ in range(n_channels_in):
             W_flat = flat_weights[c][c_]
             I_c = inputs_per_channel[c_]
+            if print_counter <= max_print:
+                print 'I_c:\n----------------------------------'
+                print I_c
+                print '----------------------------------------'
+                print 'W_flat:\n-------------------------------'
+                print W_flat
+            print_counter += 1
             r = I_c.dot(W_flat)
             r = r.reshape(output_shape[:-1])
             output_for_c += r
@@ -110,13 +224,28 @@ def conv2d(options, inputs, weights, bias, output):
     sys.stdout.write('scaling results ... ')
     sys.stdout.flush()
 
+    multiplier = (weights_S * inputs_S) / output_S
+    # def f(v):
+    #     x = rounded_mult(v, multiplier)
+    #     x += output_Z
+    #     x = 255 if x > 255 else x
+    #     return 0 if x < 0 else x
+
+    # f = np.vectorize(f)
+
+    # output_final = np.array(output_list)
+    # output_final = np.expand_dims(f(output_final), axis=0)
+    # print output_final.shape
+    # return output_final
+
     # this step is very expensive (probably the clamping and whatnot)
     output_final = np.zeros((output.shape))
     for c in range(n_channels_out):
         for i in range(output_shape[0]):
             for j in range(output_shape[1]):
                 v = output_list[c][i][j]
-                v = mult_by_quant_mult(v, qm, n)
+                v = rounded_mult(v, multiplier)
+                # v = mult_by_quant_mult(v, qm, n)
                 v += output_Z
                 v = 255 if v > 255 else v
                 v = 0 if v < 0 else v
@@ -124,7 +253,6 @@ def conv2d(options, inputs, weights, bias, output):
 
     print 'done'
 
-    print output_final
     return output_final
 
 
@@ -142,6 +270,8 @@ def dwconv2d(options, inputs, weights, bias, output):
     output_S, output_Z = output.scale, output.zero_point
 
     n, qm = quantization.compute_multiplier_for_conv2d(weights_S, inputs_S, output_S)
+
+    multiplier = (weights_S * inputs_S) / output_S
 
     # other options
     stride_h, stride_w = options.stride
@@ -174,12 +304,13 @@ def dwconv2d(options, inputs, weights, bias, output):
                         xx = x + a - padding_h
                         yy = y + b - padding_w
                         if (0 <= xx < inputs_h) and (0 <= yy < inputs_w):
-                            z.append(inputs[0][xx][yy][c] - inputs_Z)
+                            z.append(int64(inputs[0][xx][yy][c] - inputs_Z))
                         else:
-                            z.append(0)
+                            z.append(int64(0))
                 Z.append(z)
         inputs_per_channel.append(Z)
-    inputs_per_channel = np.array(inputs_per_channel, dtype='int32')
+    inputs_per_channel = np.array(inputs_per_channel, dtype='int64')
+    print inputs_per_channel.shape
 
     print 'done'
 
@@ -198,7 +329,7 @@ def dwconv2d(options, inputs, weights, bias, output):
             for a in range(weights_h):
                 for b in range(weights_w):
                     w.append(weights[d][a][b][c_] - weights_Z)
-            ww.append(np.array(w, dtype='int32'))
+            ww.append(np.array(w, dtype='int64'))
         flat_weights.append(ww)
 
     print 'done'
@@ -206,11 +337,21 @@ def dwconv2d(options, inputs, weights, bias, output):
     sys.stdout.write('computing dwconv2d ... ')
     sys.stdout.flush()
 
+    max_print = 10
+    print_counter = 0
+
     output_list = list()
     for d in range(depth_mult):
         for c in range(n_channels_in):
             W_flat = flat_weights[d][c_]
             I_c = inputs_per_channel[c_]
+            if print_counter <= max_print:
+                print 'I_c:\n----------------------------------'
+                print I_c
+                print '----------------------------------------'
+                print 'W_flat:\n-------------------------------'
+                print W_flat
+            print_counter += 1
             r = I_c.dot(W_flat).reshape(output_shape[1:-1])
             r += bias[c]
             output_list.append(r)
@@ -225,18 +366,17 @@ def dwconv2d(options, inputs, weights, bias, output):
         for i in range(output_shape[1]):
             for j in range(output_shape[2]):
                 v = output_list[c][i][j]
-                v = mult_by_quant_mult(v, qm, n)
+                # v = mult_by_quant_mult(v, qm, n)
+                v = rounded_mult(v, multiplier)
                 v += output_Z
                 v = 255 if v > 255 else v
                 v = 0 if v < 0 else v
                 output_final[0][i][j][c] = v
 
     print 'done'
-    print output_final
     return output_final
 
-two_23 = pow(2, 23)
-int32 = np.int32
+
 
 def add(options, input1, input2, output):
     # TODO: Doing something wrong here....
@@ -289,20 +429,48 @@ def avgpool2d(options, input, output):
     n = input_h * input_w
     print 'divisor: ', n
 
+    assert options.padding == 'VALID'
     assert output_h == output_w == 1
     assert n_channels_in == n_channels_out
 
-    output_ = np.zeros(output.shape)
+    padding_h, padding_w = (0, 0)
+    stride_h, stride_w = options.stride
+    filter_h, filter_w = options.filter_size
 
-    for c in range(n_channels_out):
-        acc = int32(0)
-        for i in range(input_h):
-            for j in range(input_w):
-                acc += input[0][i][j][c] - input.zero_point
-        avg = (float(acc) / n)
-        output_[0][0][0][c] = int(round(avg))
+    output_ = np.zeros(output.shape, dtype='float64')
 
+    for out_y in range(output_h):
+        for out_x in range(output_w):
+            for c in range(n_channels_in):
+                in_x_origin = (out_x * stride_w) - padding_w
+                in_y_origin = (out_y * stride_h) - padding_h
+                fxs = max(0, -in_x_origin)
+                fxe = min(filter_w, input_w - in_x_origin)
+                fys = max(0, -in_y_origin)
+                fye = min(filter_h, input_h - in_y_origin)
+                total = 0.0
+                fc = 0
+                for filter_y in range(fys, fye):
+                    for filter_x in range(fxs, fxe):
+                        in_x = in_x_origin + filter_x
+                        in_y = in_y_origin + filter_y
+                        total += input[0][in_y][in_x][c]
+                        fc +=1
+                avg = total / fc
+                output_[0][out_y][out_x][c] = avg
+
+    output.data = output_
     return output_
+
+    # for c in range(n_channels_out):
+    #     acc = np.int64(0)
+    #     for i in range(input_h):
+    #         for j in range(input_w):
+    #             acc += int64(input[0][i][j][c] - input.zero_point)
+    #     avg = (float(acc) / n)
+    #     output_[0][0][0][c] = int(round(avg))
+
+    # return output_
 
 
 def load_image(image_path, input_shape):
@@ -328,28 +496,41 @@ def run(model_path, input_image):
     model.set_input(load_image(input_image, input_shape))
 
     for op in model:
+        print '-------------------------------------'
         print op
         inputs = model.get_named_inputs_for_op(op)
         output = list(model.get_outputs_for_op(op))[0]  # assume single output
 
+        # print 'inputs:'
+        # print inputs
+        # print 'output:'
+        # print output
+
+        # raw_input('---')
+
+        if inputs['_'][0].name == 'input':
+            inputs['_'][0].data = dequantize_tensor(inputs['_'][0])
+
         opname = op.opname
         if 'CONV_2D' == opname:
-            output.data = np.random.randint(0,255,size=output.shape)
+            # output.data = np.random.randint(0,255,size=output.shape)
             # output.data = np.zeros(output.shape)
-            x = conv2d(op,
+            x = conv2d_dequant(op,
                        inputs['_'][0],
                        inputs['weights'][0],
                        inputs['bias'][0],
                        output)
+            print x
             output.data = x
         elif 'DEPTHWISE_CONV_2D' == opname:
-            output.data = np.random.randint(0,255,size=output.shape)
+            # output.data = np.random.randint(0,255,size=output.shape)
             # output.data = np.zeros(output.shape)
-            x = dwconv2d(op,
+            x = dwconv2d_dequant(op,
                          inputs['_'][0],
                          inputs['weights'][0],
                          inputs['bias'][0],
                          output)
+            print x
             output.data = x
         elif 'ADD' == opname:
 #             output.data = np.random.randint(0,255,size=output.shape)
@@ -365,8 +546,6 @@ def run(model_path, input_image):
                     output)
             output.data = x
             print (input1.scale*(input1.data - input1.zero_point) + input2.scale*(input2.data - input2.zero_point))[0][0][0] - output.scale*(output.data - output.zero_point)[0][0][0]
-
-#            return model
         elif 'AVERAGE_POOL_2D' == opname:
             x = avgpool2d(op,
                           inputs['_'][0],
@@ -387,5 +566,7 @@ def run(model_path, input_image):
     return model
 
 if __name__ == '__main__':
-    assert len(sys.argv) == 3
+    if len(sys.argv) < 3:
+        print 'Usage: %s [tflite file] [image]' % (sys.argv[0],)
+        exit(0)
     model = run(sys.argv[1], sys.argv[2])
