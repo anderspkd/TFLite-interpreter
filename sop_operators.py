f0bda330a6f38cc2b8aedb8a7ae108e9ce215356
Anders Dalskov <anderspkd@fastmail.com>
2019-02-01 11:11:26 +0100

    Sum-of-product operators.

--- sop_operators.py
+++ sop_operators.py
@@ -1,4 +1,4 @@
-# sop ops
+# Sum-of-Product operators
 #
 # Implementation of the convolution etc operators that focus on a
 # sum-of-products style approach.
@@ -10,7 +10,6 @@ import quantization
 import numpy as np
 from sys import stdout
 
-# np.set_printoptions(threshold=np.nan)
 
 def p(string):
     stdout.write(string)
@@ -27,7 +26,7 @@ def offset_with_size(dim0, dim1, dim2):
 def _flatten_weights(weights, shape):
     # helper function for conv2d and dwconv2d
     oc, wh, ww, ic = shape
-    data = np.zeros((oc, ic * wh * ww))
+    data = np.zeros((oc, ic * wh * ww), dtype='int64')
     offset = offset_with_size(ic, wh, ww)
     for c in range(oc):
         for c_ in range(ic):
@@ -41,24 +40,26 @@ def _get_window_for_channel(inputs, x, y, size_x, size_y, channel):
     # returns the window in channel `channel` of `inputs` with origin `x` and
     # `y`. `size_x` and `size_y` denote the size of the window. If the window
     # happen to be out of bounds, we pad with 0s
-    window = np.zeros((size_x * size_y, ))
+    window = list()
     max_x = inputs.shape[2]
     max_y = inputs.shape[1]
-    for i in range(size_x):
-        for j in range(size_y):
+    for j in range(size_y):
+        for i in range(size_x):
             x_ = x + i
             y_ = y + j
             if (0 <= x_ < max_x) and (0 <= y_ < max_y):
-                window[j * size_y + i] = inputs[0][y][x][channel]
-    return window
+                window.append(inputs[0][y_][x_][channel])
+            else:
+                window.append(0)
+    return np.array(window, dtype='int64')
 
 
 def _get_full_window(inputs, x, y, size_x, size_y):
     # as above, except we extract all windows across all input channels
     windows = list()
     for c in range(inputs.shape[3]):
-        windows.append(_get_window_for_channel(
-            inputs, x, y, size_x, size_y, c))
+        window = _get_window_for_channel(inputs, x, y, size_x, size_y, c)
+        windows.append(window)
     return np.hstack(windows)
 
 
@@ -71,7 +72,7 @@ def conv2d(options, inputs, weights, bias, output):
     # the _conv2d function. Of course, this can be performed in a preprocessing
     # step.
     p('flattening weights ....')
-    _wd = np.array(weights.data, dtype='int32')
+    _wd = np.array(weights.data, dtype='int64')
     wd = _flatten_weights(_wd - weights.zero_point, weights.shape)
     print 'done'
     p('computing conv2d ....')
@@ -103,7 +104,7 @@ def dwconv2d(options, inputs, weights, bias, output):
         weights.scale, inputs.scale, output.scale
     )
     padding_h, padding_w = (weights.shape[1] // 2, weights.shape[2] // 2)
-    wd = np.array(weights.data, dtype='int32')
+    wd = np.array(weights.data, dtype='int64')
     p('computing depthwise convolution ....')
     x = _dwconv2d(
         # input
@@ -128,6 +129,18 @@ def dwconv2d(options, inputs, weights, bias, output):
     return x
 
 
+def avgpool2d(options, inputs, output):
+    p('computing average pool ....')
+    x = _avgpool2d(inputs.data,
+                   inputs.shape,
+                   output.shape,
+                   options.stride,
+                   options.filter_size
+    )
+    print 'done'
+    return x
+
+
 qmult = quantization.quantized_multiplier_mult
 
 def _conv2d(input_data, input_offset, input_shape, output_offset, output_shape,
@@ -135,15 +148,14 @@ def _conv2d(input_data, input_offset, input_shape, output_offset, output_shape,
             multiplier):
     output_w = output_shape[2]
     output_h = output_shape[1]
-    output_data = np.zeros(output_shape)
+    output_data = np.zeros(output_shape, dtype='int64')
     for out_y in range(output_h):
         for out_x in range(output_w):
             for out_c in range(output_shape[3]):
                 x = (out_x * stride[0]) - padding[0]
                 y = (out_y * stride[1]) - padding[1]
-                window = _get_full_window(input_data, x, y, weights_shape[1],
+                window = _get_full_window(input_data - input_offset, x, y, weights_shape[1],
                                           weights_shape[2])
-                window -= input_offset
                 # compute sum-of-products (dot product)
                 z = window.dot(weights_data[out_c])
                 # add bias
@@ -163,7 +175,7 @@ def _flat_filter_for_channel(weights, channel):
     # _dwconv2d.
     height = weights.shape[1]
     width = weights.shape[2]
-    fw = np.zeros((height * width))
+    fw = np.zeros((height * width), dtype='int64')
     for x in range(width):
         for y in range(height):
             # assume depth multiplier == 1
@@ -176,7 +188,7 @@ def _dwconv2d(input_data, input_offset, input_shape, output_offset,
               padding, shift, multiplier):
     output_h = output_shape[1]
     output_w = output_shape[2]
-    output_data = np.zeros(output_shape)
+    output_data = np.zeros(output_shape, dtype='int64')
     for out_y in range(output_h):
         for out_x in range(output_w):
             for in_c in range(input_shape[3]):
@@ -196,22 +208,11 @@ def _dwconv2d(input_data, input_offset, input_shape, output_offset,
     return output_data
 
 
-def avgpool2d(options, inputs, output):
-    p('computing average pool ....')
-    x = _avgpool2d(inputs.data,
-                   inputs.shape,
-                   output.shape,
-                   options.stride,
-                   options.filter_size
-    )
-    print 'done'
-    return x
-
-
 def _avgpool2d(input_data, input_shape, output_shape, stride, filter_size):
+    output_data = np.zeros(output_shape)
     for out_y in range(output_shape[1]):
         for out_x in range(output_shape[2]):
-            for in_c in range(input_shape[3]):
+            for c in range(input_shape[3]):
                 # assume VALID padding
                 x = out_x * stride[0]
                 y = out_y * stride[1]
@@ -219,7 +220,18 @@ def _avgpool2d(input_data, input_shape, output_shape, stride, filter_size):
                 fxe = min(filter_size[1], input_shape[2] - x)
                 fye = min(filter_size[0], input_shape[1] - y)
                 acc = 0
-                div = 1
+                div = 0
+                for fy in range(0, fye):
+                    for fx in range(0, fxe):
+                        x_ = x + fx
+                        y_ = y + fy
+                        acc += input_data[0][y_][x_][c]
+                        div += 1
+                acc = (acc + div / 2) / div
+                acc = min(255, max(0, acc))
+                output_data[0][out_y][out_x][c] = acc
+    return output_data
+
 
 if __name__ == '__main__':
     np.set_printoptions(threshold=np.nan)
